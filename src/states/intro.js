42a4fc3ef73fa7b789770ff59c46c22e51a72126
bodiddlie <bodiddlie@gmail.com>
2016-08-08 16:13:27 -0700

    finished intro, moved talking heads to own module, started MS state

--- src/states/intro.js
+++ src/states/intro.js
@@ -1,132 +1,136 @@
 import Phaser from 'phaser';
 
-const startFont = {
-  font: '15px Arial',
-  fill: '#FFFFFF',
-  align: 'center'
-};
+import TalkingHeads from '../talking-heads';
 
 let scene = [
-    {
-        speaking: 'jeff',
-        visible: ['jeff'],
-        text: 'Hello, and welcome to Giant Bomb\'s E3 2175 coverage.'
-    },
-    {
-        speaking: 'jeff',
-        visible: ['jeff'],
-        text: 'I\'m Jeff Gerstmann and I\'m joined today by Mr. Vinny Caravela...'
-    },
-    {
-        speaking: 'vinny',
-        visible: ['vinny'],
-        text: 'I think I went into the wrong building...'
-    },
-    {
-        speaking: 'jeff',
-        visible: ['jeff'],
-        text: 'Always a pleasure, Vinny.'
-    },
-    {
-        speaking: 'jeff',
-        visible: ['jeff'],
-        text: 'Also with us is Transhuman AI Brad Shoemaker...'
-    },
-    {
-        speaking: 'brad',
-        visible: ['brad'],
-        text: 'I AM NOW PRESENT....'
-    },
-    {
-        speaking: 'jeff',
-        visible: ['jeff'],
-        text: 'Ominous as ever Brad.'
-    },
-    {
-        speaking: 'jeff',
-        visible: ['jeff'],
-        text: 'Next is Genetically Augmented Chalupa Dan Ryckert...'
-    },
-    {
-        speaking: 'dan',
-        visible: ['dan'],
-        text: 'Grrdalkrlakfalkrjalrlakdfjlakrlakrjflsslslrjs....'
-    },
-    {
-        speaking: 'jeff',
-        visible: ['jeff'],
-        text: 'I think we might need more Taco Bell wrappers under Dan to keep it clean in here.'
-    },
-    {
-        speaking: 'jeff',
-        visible: ['jeff'],
-        text: 'And at the end of the row is the disembodied cybernetic head of Alex Navaro....'
-    },
-    {
-        speaking: 'alex',
-        visible: ['alex'],
-        text: 'It has been 150 years and still I know nothing but pain....'
-    }
+  {
+    speaking: 'jeff',
+    visible: ['jeff'],
+    text: 'Hello, and welcome to Giant Bomb\'s E3 2175 coverage.'
+  },
+  {
+    speaking: 'jeff',
+    visible: ['jeff'],
+    text: 'I\'m Jeff Gerstmann and I\'m joined today by Mr. Vinny Caravela...'
+  },
+  {
+    speaking: 'vinny',
+    visible: ['vinny'],
+    text: 'I think I went into the wrong building...'
+  },
+  {
+    speaking: 'jeff',
+    visible: ['jeff'],
+    text: 'Always a pleasure, Vinny.'
+  },
+  {
+    speaking: 'jeff',
+    visible: ['jeff'],
+    text: 'Also with us is Transhuman AI Brad Shoemaker...'
+  },
+  {
+    speaking: 'brad',
+    visible: ['brad'],
+    text: 'I AM NOW PRESENT....'
+  },
+  {
+    speaking: 'jeff',
+    visible: ['jeff'],
+    text: 'Ominous as ever Brad.'
+  },
+  {
+    speaking: 'jeff',
+    visible: ['jeff'],
+    text: 'Next is Genetically Augmented Chalupa Dan Ryckert...'
+  },
+  {
+    speaking: 'dan',
+    visible: ['dan'],
+    text: 'Grrdalkrlakfalkrjalrlakdfjlakrlakrjflsslslrjs....'
+  },
+  {
+    speaking: 'jeff',
+    visible: ['jeff'],
+    text: 'I think we might need more Taco Bell wrappers under Dan to keep it clean in here.'
+  },
+  {
+    speaking: 'jeff',
+    visible: ['jeff'],
+    text: 'And at the end of the row is the disembodied cybernetic head of Alex Navaro....'
+  },
+  {
+    speaking: 'alex',
+    visible: ['alex'],
+    text: 'It has been 150 years and still I know nothing but pain....'
+  },
+  {
+    speaking: 'jeff',
+    visible: ['jeff'],
+    text: 'A ray of sunshine, absolutely uplifting.'
+  },
+  {
+    speaking: 'jeff',
+    visible: ['jeff', 'vinny', 'brad', 'dan', 'alex'],
+    text: 'We\'re all here today to talk over the various press conferences as they happen.'
+  },
+  {
+    speaking: 'jeff',
+    visible: ['jeff', 'vinny', 'brad', 'dan', 'alex'],
+    text: 'First up today is Microsoft. What are we expecting to see from them? Any surprises expected?'
+  },
+  {
+    speaking: 'brad',
+    visible: ['jeff', 'vinny', 'brad', 'dan', 'alex'],
+    text: 'NEW HALO ANNOUNCED. MASTER CHIEF IS A FRAUD. I AM THE TRUE MERGING OF HUMAN AND TECHNOLOGY.'
+  },
+  {
+    speaking: 'dan',
+    visible: ['jeff', 'vinny', 'brad', 'dan', 'alex'],
+    text: 'Gralskdjasf....Viva Pinata....falskjdflasf.....'
+  },
+  {
+    speaking: 'vinny',
+    visible: ['jeff', 'vinny', 'brad', 'dan', 'alex'],
+    text: 'I just hope they finally have some kind of Kinect based woodworking simulator.'
+  },
+  {
+    speaking: 'alex',
+    visible: ['jeff', 'vinny', 'brad', 'dan', 'alex'],
+    text: 'Phil Spencer will open the hellmaw and swallow us all whole. Or maybe a new Gears of War. I dunno.'
+  },
+  {
+    speaking: 'jeff',
+    visible: ['jeff', 'vinny', 'brad', 'dan', 'alex'],
+    text: 'Well the house lights are down and it looks like the conference is starting. Let\'s see what\'s up with Microsoft!'
+  }
 ];
 
 export class Intro extends Phaser.State {
-    preload() {
-        this.load.spritesheet('jeff', 'assets/staff-sprites/jeff.png', 160, 160, 2);
-        this.load.spritesheet('vinny', 'assets/staff-sprites/vinny.png', 160, 160, 2);
-        this.load.spritesheet('brad', 'assets/staff-sprites/brad.png', 160, 160, 2);
-        this.load.spritesheet('dan', 'assets/staff-sprites/dan.png', 160, 160, 2);
-        this.load.spritesheet('alex', 'assets/staff-sprites/alex.png', 160, 160, 2);
+  init() {
+    this.heads = new TalkingHeads(this.load, this.add);
+  }
+
+  preload() {
+    this.heads.preload();
+  }
+
+  create() {
+    this.heads.create();
+    this.input.onDown.add(this.cycleText, this);
+
+    this.textIndex = 0;
+    this.heads.setVisible(scene[this.textIndex].visible);
+    this.heads.setSpeaking(scene[this.textIndex].speaking, scene[this.textIndex].text);
+  }
+
+  cycleText() {
+    if (this.textIndex < scene.length - 1) {
+      this.textIndex++;
+      const sceneInfo = scene[this.textIndex];
+      this.heads.setVisible(sceneInfo.visible);
+      this.heads.setSpeaking(sceneInfo.speaking, sceneInfo.text);
+    } else {
+      this.game.state.start('Microsoft');
     }
-
-    create() {
-      let graphics = this.add.graphics(5, this.world.height - 100);
-      graphics.lineStyle(5, 0xFFFFFF, 1);
-      graphics.drawRoundedRect(0, 0, this.world.width - 10, 95, 5);
-
-      this.textIndex = 0;
-      this.textObject = this.add.text(15, this.world.height - 85, scene[this.textIndex].text, startFont);
-
-      this.input.onDown.add(this.cycleText, this);
-
-      this.sprites = {};
-
-      this.sprites.jeff = this.add.sprite(0, 340, 'jeff');
-      this.jeffTalk = this.sprites.jeff.animations.add('talk');
-      this.sprites.jeff.animations.play('talk', 3, true);
-
-      this.sprites.vinny = this.add.sprite(160, 340, 'vinny');
-      this.vinnyTalk = this.sprites.vinny.animations.add('talk');
-      this.sprites.vinny.visible = false;
-      //this.vinny.animations.play('talk', 3, true);
-
-      this.sprites.brad = this.add.sprite(320, 340, 'brad');
-      this.bradTalk = this.sprites.brad.animations.add('talk');
-      this.sprites.brad.visible = false;
-      //this.brad.animations.play('talk', 3, true);
-
-      this.sprites.dan = this.add.sprite(480, 340, 'dan');
-      this.danTalk = this.sprites.dan.animations.add('talk');
-      this.sprites.dan.visible = false;
-      //this.dan.animations.play('talk', 3, true);
-
-      this.sprites.alex = this.add.sprite(640, 340, 'alex');
-      this.alexTalk = this.sprites.alex.animations.add('talk');
-      this.sprites.alex.visible = false;
-      //this.alex.animations.play('talk', 3, true);
-    }
-
-    cycleText() {
-      if (this.textIndex < scene.length - 1) {
-          this.textIndex++;
-          const sceneInfo = scene[this.textIndex];
-          this.textObject.setText(sceneInfo.text);
-
-          for (let name in this.sprites) {
-              this.sprites[name].visible = false;
-              this.sprites[name].animations.stop();
-          }
-          sceneInfo.visible.forEach(name => this.sprites[name].visible = true);
-          this.sprites[sceneInfo.speaking].animations.play('talk', 3, true);
-      }
-    }
+  }
 }
---
+++ src/talking-heads.js
@@ -0,0 +1,68 @@
+const dialogFont = {
+    font: '15px Arial',
+    fill: '#FFFFFF',
+    align: 'left',
+    wordWrap: true,
+    wordWrapWidth: 770
+};
+
+export default class TalkingHeads {
+    constructor(load, add) {
+        this.load = load;
+        this.add = add;
+    }
+
+    preload() {
+        this.load.spritesheet('jeff', 'assets/staff-sprites/jeff.png', 160, 160, 2);
+        this.load.spritesheet('vinny', 'assets/staff-sprites/vinny.png', 160, 160, 2);
+        this.load.spritesheet('brad', 'assets/staff-sprites/brad.png', 160, 160, 2);
+        this.load.spritesheet('dan', 'assets/staff-sprites/dan.png', 160, 160, 2);
+        this.load.spritesheet('alex', 'assets/staff-sprites/alex.png', 160, 160, 2);
+    }
+
+    create() {
+        let graphics = this.add.graphics(5, 500);
+        graphics.lineStyle(5, 0xFFFFFF, 1);
+        graphics.drawRoundedRect(0, 0, 790, 95, 5);
+
+        let bg = this.add.graphics(0, 340);
+        bg.beginFill(0x8800FF);
+        bg.drawRect(0, 0, 800, 160);
+
+        this.textObject = this.add.text(15, 515, '', dialogFont);
+
+        this.sprites = {};
+
+        this.sprites.jeff = this.add.sprite(0, 340, 'jeff');
+        this.jeffTalk = this.sprites.jeff.animations.add('talk');
+
+        this.sprites.vinny = this.add.sprite(160, 340, 'vinny');
+        this.vinnyTalk = this.sprites.vinny.animations.add('talk');
+
+        this.sprites.brad = this.add.sprite(320, 340, 'brad');
+        this.bradTalk = this.sprites.brad.animations.add('talk');
+
+        this.sprites.dan = this.add.sprite(480, 340, 'dan');
+        this.danTalk = this.sprites.dan.animations.add('talk');
+
+        this.sprites.alex = this.add.sprite(640, 340, 'alex');
+        this.alexTalk = this.sprites.alex.animations.add('talk');
+    }
+
+    setVisible(whoAllIsVisible) {
+        if (whoAllIsVisible) {
+            for (let name in this.sprites) {
+                this.sprites[name].visible = false;
+            }
+            whoAllIsVisible.forEach(name => this.sprites[name].visible = true);
+        }
+    }
+
+    setSpeaking(speaker, text) {
+        for (let name in this.sprites) {
+            this.sprites[name].animations.stop(null, true);
+        }
+        this.sprites[speaker].animations.play('talk', 3, true);
+        this.textObject.setText(speaker.toUpperCase() + ':\n' + text);
+    }
+}
