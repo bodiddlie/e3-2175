4183faabb3418044699b1520393c6969310b727e
Nick Klepinger <bodiddlie@gmail.com>
2016-08-07 19:18:38 -0700

    added ternjs stuff, started some text intro messages

--- src/states/intro.js
+++ src/states/intro.js
@@ -6,20 +6,34 @@ const startFont = {
   align: 'center'
 };
 
+let text = [
+    'Hello, and welcome to Giant Bomb\'s E3 2175 coverage.',
+    'I\'m Jeff Gerstmann and I\'m joined today by Vinny Caravela,',
+    'Hello.....',
+    'Transhuman AI Brad Shoemaker,',
+    'I AM NOW PRESENT....',
+    'Genetically engineered Chalupa Dan Rykert',
+    'Grrrbtlalrkkbhtrkrkalrkfkrkjrrkf......',
+    'And the disembodied head of Alex Navaro,',
+    'It has been 150 years and still I know nothing but pain...'
+];
+
 export class Intro extends Phaser.State {
   create() {
       let graphics = this.add.graphics(5, this.world.height - 100);
       graphics.lineStyle(5, 0xFFFFFF, 1);
       graphics.drawRoundedRect(0, 0, this.world.width - 10, 95, 5);
 
-      let start = 'Hello, and welcome to Giant Bomb\'s E3 2175 coverage.';
-      let startText = this.add.text(15, this.world.height - 85, start, startFont);
-      //startText.anchor.set(0.5, 0.5);
+      this.textIndex = 0;
+      this.textObject = this.add.text(15, this.world.height - 85, text[this.textIndex], startFont);
 
-      //this.input.onDown.addOnce(this.startGame, this);
+      this.input.onDown.add(this.cycleText, this);
   }
 
-  startGame() {
-      //this.game.state.start('TestTile');
+  cycleText() {
+      if (this.textIndex < text.length - 1) {
+          this.textIndex++;
+          this.textObject.setText(text[this.textIndex]);
+      }
   }
 }
